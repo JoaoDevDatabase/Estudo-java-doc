6fdd6a7777463d7e91ca235a006926097d47b5b3
João Vitor Stellet <joaovitorstellet@gmail.com>
2023-07-14 17:16:20 -0300

    tentativa de utilizar a API JAVA DOC

---
+++ EstudoJavaDoc/EstudoJavadoc.java
@@ -0,0 +1,32 @@
+package EstudoJavaDoc;
+
+public class EstudoJavadoc {
+    public static void main(String[] args) {
+        /*
+         * @author João Vitor Stellet
+         * 
+         * @version 1.0
+         * 
+         * @since 13/07/2023
+         * 
+         */
+        // vamos fazer uma aplicação que vamos ter que utilizar tags de "ETIQUETAS"
+        // utilizando o java docs;
+
+        // Vamos fazer uma aplicação de falar que o numero e maior ou menor;
+
+        // O JAVA DOC E UM GERADOR DE DOCUMENTAÇÃO, UMA API PARA PODERMOS USAR COMO
+        // DOCUMENTAÇÃO .
+
+        int numero = 5;
+        int numero2 = 5;
+
+        if (numero == numero2) {
+            System.out.println("NÚMEROS IGUAIS");
+        } else {
+            System.out.println("OS NÚMEROS NÃO SÃO IGUAIS!");
+        }
+
+    }
+}
+// erro encontrado na api de documentação;
